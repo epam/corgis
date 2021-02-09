9a050b6c31b4ca40420714e9ea5414dbae6c85a6
ohexus <moskalenko.taras.d@gmail.com>
2021-02-09 17:11:54 +0200

    refactored with new Dropdown and GenerationLink components, added Nav.scss and moved all styles to it

--- src/component/Header/Nav/Nav.jsx
+++ src/component/Header/Nav/Nav.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import './Nav.scss';
+
 import Button from '../../utils/Button/Button';
-import IconNav from '../../../assets/images/icon-nav.svg';
+import Dropdown from '../../Dropdown/Dropdown';
+import GenerationLink from './GenerationLink/GenerationLink';
 
 const NavPropTypes = {
   number: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
@@ -12,142 +15,23 @@ const NavPropTypes = {
 };
 
 const Nav = ({ number, accountName, requestSignOut }) => (
-  <div className='wrap'>
-    <div className='account'>
+  <nav className='nav'>
+    <div className='nav__account'>
       <NavLink to='/account'>
         <Button description={`My Corgis ( ${number} )`} />
       </NavLink>
-      <Card accountName={accountName} requestSignOut={requestSignOut} />
+      <Dropdown dropdownTitle={`@${accountName}▾`}>
+        <NavLink to='/profile'>
+          <button>Edit Profile</button>
+        </NavLink>
+        <button onClick={requestSignOut}>Sign Out</button>
+      </Dropdown>
     </div>
     <NavLink to='/generation'>
-      <AddCorgi />
+      <GenerationLink />
     </NavLink>
-    <style>{`
-        .wrap {
-          margin: auto;
-          padding: 5px 30px;
-          display: inline-flex;
-          font-size: 1em;
-        }
-        
-        .account{
-          display: inline-flex;
-          flex-direction: row;
-        }
-        
-        .menutop {
-          border-radius: 5px;
-          border: 2px solid #fbb040;
-          background-color: #fff;
-          padding: 5px 30px;
-          box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
-          cursor: grabbing;
-        }
-        
-        .dropdown {
-          position: relative;
-          display: inline-block;
-          margin: 0 15px;
-        }
-        
-        .dropdown-content {
-          display: none;
-          position: absolute;
-          background-color: #fdfcfc;
-          width: 100%;
-          z-index: 1;
-          box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
-          border-radius: 0 0 5px 5px;
-        }
-        
-        .dropdown:hover .dropdown-content {
-          display: block;
-        }
-        
-        .menutop:hover .menutop {
-            border: none;
-            border-radius: 5px 5px 0 0;
-        }
-        
-        li.active {
-          background: #dddddd;
-        }
-        
-        .orange {
-          color: #fbb040;
-          display: inline;
-        }
-        
-        @media all and (max-width: 1200px) {
-          .wrap{
-            font-size: 0.8em;
-          }
-          .menutop {
-            padding: 5px;
-          }
-        }
-        
-        @media all and (max-width: 450px) {
-          .wrap{
-            font-size: 0.6em;
-          }
-          .account{
-            flex-direction: row;
-          }
-          .menutop {
-            padding: 8px 3px;
-          }
-        }
-      `}</style>
-  </div>
+  </nav>
 );
 Nav.propTypes = NavPropTypes;
-
-const CardPropTypes = {
-  accountName: PropTypes.string.isRequired,
-  requestSignOut: PropTypes.func.isRequired,
-};
-
-const Card = ({ accountName, requestSignOut }) => {
-  const style = {
-    textDecoration: 'none',
-    display: 'block',
-    padding: 'auto',
-    color: '#01c9fd',
-    margin: '2%  auto',
-    cursor: 'alias',
-  };
-  return (
-    <div className='dropdown'>
-      <button className='menutop'>@{accountName}▾</button>
-      <div className='dropdown-content'>
-        <ul style={{ textAlign: 'center', padding: '2px', marginBottom: '0' }}>
-          <li style={style}>
-            <NavLink to='/profile'>
-              <button>Edit Profile</button>
-            </NavLink>
-          </li>
-          <li style={style}>
-            <button onClick={requestSignOut}>Sign Out</button>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-Card.propTypes = CardPropTypes;
-
-const AddCorgi = () => (
-  <div
-    style={{
-      boxShadow: '0 2px 2px 0 rgba(0, 0, 0, 0.5)',
-      borderRadius: '50%',
-      height: '40px',
-      width: '40px',
-    }}
-  >
-    <img src={IconNav} alt='' />
-  </div>
-);
 
 export default Nav;
