c1a82aafd15f536fc8cfcc5f36c55ef173024851
ohexus <moskalenko.taras.d@gmail.com>
2021-02-09 14:02:19 +0200

    moved Egg.jsx to a separate folder, added Egg.scss and moved all styles to it

--- src/component/utils/Egg/Egg.jsx
+++ src/component/utils/Egg/Egg.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 
-import { CorgiType } from '../../types/CorgiTypes';
+import './Egg.scss';
+
+import { CorgiType } from '../../../types/CorgiTypes';
 
 const EggPropTypes = { color: CorgiType.color };
 
 const Egg = ({ color }) => (
-  <div
-    style={{
-      width: '100%',
-      height: '100%',
-      margin: 'auto',
-      position: 'relative',
-      zIndex: '10',
-    }}
-  >
+  <div className='egg'>
     <svg
       width='147px'
       height='146px'
