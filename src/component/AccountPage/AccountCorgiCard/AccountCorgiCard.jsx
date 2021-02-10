f6294a10c990d79cafb9458e9a7369960fe94580
ohexus <moskalenko.taras.d@gmail.com>
2021-02-10 19:19:41 +0200

    added AccountCorgiCard.scss and moved all styles to it, implemented BEM

--- src/component/AccountPage/AccountCorgiCard/AccountCorgiCard.jsx
+++ src/component/AccountPage/AccountCorgiCard/AccountCorgiCard.jsx
@@ -1,69 +1,30 @@
 import React from 'react';
 
+import './AccountCorgiCard.scss';
+
 import Corgi from '../../CorgiCard/Corgi/Corgi';
 import { Dialogue } from '../../CorgiCard/Card';
 
 import { CorgiTypeShape } from '../../../types/CorgiTypes';
 
-const AccountCardPropTypes = { corgi: CorgiTypeShape };
+const AccountCorgiCardPropTypes = { corgi: CorgiTypeShape };
 
-const AccountCard = ({ corgi }) => (
-  <div className='creation'>
-    <div className='corgiboard'>
-      <div
-        style={{
-          backgroundColor: corgi.backgroundColor,
-          borderRadius: '10px',
-          padding: '20px',
-          display: 'inline-block',
-        }}
-      >
-        <Dialogue quote={corgi.quote} color={corgi.color} />
-        <Corgi color={corgi.color} sausage={corgi.sausage} />
-      </div>
+const AccountCorgiCard = ({ corgi }) => (
+  <div className='account-corgi'>
+    <div
+      className='account-corgi__card'
+      style={{
+        backgroundColor: corgi.backgroundColor,
+      }}
+    >
+      <Dialogue quote={corgi.quote} color={corgi.color} />
+      <Corgi color={corgi.color} sausage={corgi.sausage} />
     </div>
-    <p className='dogname'>{corgi.name}</p>
-    <p className='address blue'>§ {corgi.rate}</p>
-    <style>{`
-                .creation {
-                    margin: 1%;
-                    display: inline-block;
-                }
-                
-                .corgiboard {
-                    width: 300px;
-                    height: 260px;
-                    border-radius: 10px;
-                }
-                
-                .dogname {
-                    text-align: left;
-                    font-size: 1em;
-                    margin-left: 1%;
-                    margin-top: 1%;
-                    margin-bottom: 0;
-                    display: block;
-                }
-                
-                .address {
-                    text-align: left;
-                    font-size: 0.7em;
-                    margin-left: 1%;
-                    font-weight: lighter;
-                    display: block;
-                }
-                
-                .orange {
-                    color: orange;
-                }
-                
-                .blue {
-                    color: lightblue;
-                }
-            `}</style>
+    <p className='account-corgi__name'>{corgi.name}</p>
+    <p className='account-corgi__rate'>§ {corgi.rate}</p>
   </div>
 );
 
-AccountCard.propTypes = AccountCardPropTypes;
+AccountCorgiCard.propTypes = AccountCorgiCardPropTypes;
 
-export default AccountCard;
+export default AccountCorgiCard;
