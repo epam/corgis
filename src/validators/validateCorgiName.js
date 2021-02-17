81a348be82dd76449049c40a537916dffa9ecb4f
ohexus <moskalenko.taras.d@gmail.com>
2021-02-17 16:27:10 +0200

    added index.js, added new Filter() call

--- src/validators/validateCorgiName.js
+++ src/validators/validateCorgiName.js
@@ -1,13 +1,15 @@
-import BadWordsFilter from 'bad-words';
+import Filter from 'bad-words';
 
 import { CORGI_VALIDATION_MESSAGES, NAME_LIMIT } from '~constants/validation/corgi';
+
+const badWordsFilter = new Filter();
 
 export default function validateCorgiName(name) {
   if (name.length > NAME_LIMIT) {
     return CORGI_VALIDATION_MESSAGES.LIMIT;
   }
 
-  if (BadWordsFilter.isProfane(name)) {
+  if (badWordsFilter.isProfane(name)) {
     return CORGI_VALIDATION_MESSAGES.PROFANE;
   }
 
