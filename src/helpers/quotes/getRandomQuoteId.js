eab3f8836c065b41fe7c0d0f74526b6a1123f6a8
ohexus <moskalenko.taras.d@gmail.com>
2021-02-17 15:02:57 +0200

    getRandomQuoteId returns string now

--- src/helpers/quotes/getRandomQuoteId.js
+++ src/helpers/quotes/getRandomQuoteId.js
@@ -1,5 +1,5 @@
 import { quotes } from '~assets/quotes/quotes';
 
 export default function getRandomQuoteId() {
-  return Math.floor(Math.random() * quotes.length);
+  return Math.floor(Math.random() * quotes.length).toString();
 }
