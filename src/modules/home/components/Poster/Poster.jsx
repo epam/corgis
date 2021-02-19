5ea3a3e016a0dba91f320ab9fa914716cada2ab2
ohexus <moskalenko.taras.d@gmail.com>
2021-02-19 19:30:48 +0200

    replaced "-" by "&#8209;" to prevent line breaking

--- src/modules/home/components/Poster/Poster.jsx
+++ src/modules/home/components/Poster/Poster.jsx
@@ -16,15 +16,15 @@ const PosterPropTypes = {
 const Poster = ({ requestSignIn, user }) => (
   <div className='poster container-fluid'>
     <div className='row'>
-      <div className='poster__description col-xl-3'>
-        <p className='poster__text'>Create your own one-of-the-kind Corgi today</p>
+      <div className='poster__description col-xl-4'>
+        <p className='poster__text'>Create your own one&#8209;of&#8209;the&#8209;kind Corgi today</p>
 
         <p className='poster__text poster__text--small'>Mint, collect, gift and trade Corgis on Blockchain</p>
 
         {!user && <Button description='Get Started ' action={requestSignIn} />}
       </div>
 
-      <div className='poster__image col-xl-9'>
+      <div className='poster__image col-xl-8'>
         <img className='poster__corgi' src={corgiFull} alt='' />
       </div>
     </div>
