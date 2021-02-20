a8311b2b5c8ef27c9c74d28e7d9aee4391ccd68a
ohexus <moskalenko.taras.d@gmail.com>
2021-02-20 21:53:42 +0200

    replaced col-xl with col-lg

--- src/modules/home/components/Poster/Poster.jsx
+++ src/modules/home/components/Poster/Poster.jsx
@@ -16,7 +16,7 @@ const PosterPropTypes = {
 const Poster = ({ requestSignIn, user }) => (
   <div className='poster container-fluid'>
     <div className='row'>
-      <div className='poster__description col-xl-4'>
+      <div className='poster__description col-lg-4'>
         <p className='poster__text'>Create your own one&#8209;of&#8209;the&#8209;kind Corgi today</p>
 
         <p className='poster__text poster__text--small'>Mint, collect, gift and trade Corgis on Blockchain</p>
@@ -24,7 +24,7 @@ const Poster = ({ requestSignIn, user }) => (
         {!user && <Button description='Get Started ' action={requestSignIn} />}
       </div>
 
-      <div className='poster__image col-xl-8'>
+      <div className='poster__image col-lg-8'>
         <img className='poster__corgi' src={corgiFull} alt='' />
       </div>
     </div>
