ef7383d12f0214fc972f7b4f27e7ebd2465eba78
ohexus <moskalenko.taras.d@gmail.com>
2021-02-23 20:04:01 +0200

    added PopupType import, fixed open/close bug, added position to Popup

--- src/modules/common/PopupWrapper/PopupWrapper.jsx
+++ src/modules/common/PopupWrapper/PopupWrapper.jsx
@@ -1,30 +1,28 @@
 import React, { useState, useRef } from 'react';
-import PropTypes from 'prop-types';
 
 import './PopupWrapper.scss';
 
-import classNames from 'classnames';
-
-import { ReactChildrenTypeRequired } from '~types/ReactChildrenType';
-import { Popup } from '~modules/common';
 import { useDetectClickOutside } from '~hooks/';
-import { useEffect } from 'react';
 
-const PopupTypeShape = PropTypes.shape({
-  title: PropTypes.string,
-  children: ReactChildrenTypeRequired,
-});
+import { Popup } from '~modules/common';
+
+import { PopupTypeShape } from '~types/PopupTypes';
+import { ReactChildrenTypeRequired } from '~types/ReactChildrenType';
 
 const PopupWrapperPropTypes = {
   popup: PopupTypeShape,
   children: ReactChildrenTypeRequired,
 };
 
-const PopupWrapper = ({ popup = { title: '', children: <></> }, children }) => {
+const PopupWrapper = ({ popup = { title: '', position: 'top', children: <></> }, children }) => {
   const [isPopupOpened, setIsPopupOpened] = useState(false);
 
   const togglePopup = () => {
-    setIsPopupOpened(!isPopupOpened);
+    if (isPopupOpened) {
+      setIsPopupOpened(false);
+    } else {
+      setIsPopupOpened(true);
+    }
   };
 
   const handleClickOutside = () => {
@@ -35,13 +33,9 @@ const PopupWrapper = ({ popup = { title: '', children: <></> }, children }) => {
 
   useDetectClickOutside(PopupWrapperRef, handleClickOutside);
 
-  useEffect(() => {
-    console.log(isPopupOpened);
-  }, [isPopupOpened]);
-
   return (
     <div className='popup-wrapper' ref={PopupWrapperRef}>
-      <Popup isOpened={isPopupOpened} title={popup.title} position='top'>
+      <Popup isOpened={isPopupOpened} title={popup.title} position={popup.position}>
         {popup.children}
       </Popup>
 
