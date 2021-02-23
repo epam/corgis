cf0a5d1a88e77cf92bc72ba919e5d584921e414a
ohexus <moskalenko.taras.d@gmail.com>
2021-02-23 10:44:03 +0200

    fixed check for timestamp length

--- src/helpers/humanizeTime.js
+++ src/helpers/humanizeTime.js
@@ -3,6 +3,10 @@ import humanizeDuration from 'humanize-duration';
 /**
  * In JavaScript Date milliseconds are limited by 13 nums.
  * The only way to correctly calculate the date is to cut redundunt or add missing nums.
+ * 
+ * This function calculates how much time passed from the given timestamp.
+ *
+ * The following parameter is a timestamp in ms.
  * @param {(string | number)} ms
  *
  * Returns the parsed and humanized time in string representation.
@@ -15,7 +19,7 @@ export default function humanizeTime(ms) {
       if (msStr.length > 13) {
         return msStr.slice(0, 13);
       } else if (msStr.length < 13) {
-        for (let i = ms.length; i <= 13; i++) {
+        for (let i = msStr.length; i < 13; i++) {
           msStr += '0';
         }
         return msStr;
