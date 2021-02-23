5ac5cf8658d5401a8701e1614ad374a2918534c4
ohexus <moskalenko.taras.d@gmail.com>
2021-02-23 13:21:42 +0200

    refactored CorgiCard, changed corgi image height, added optional actions dropdown

--- src/modules/common/corgi/CorgiCard/CorgiCard.jsx
+++ src/modules/common/corgi/CorgiCard/CorgiCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import './CorgiCard.scss';
 
-import { CorgiSVG, Quote, RarityString } from '~modules/common';
+import { CorgiSVG, Dropdown, Quote, RarityString } from '~modules/common';
 
 import humanizeTime from '~helpers/humanizeTime';
 
@@ -11,15 +12,49 @@ import { SAUSAGE } from '~constants/corgi';
 
 import { CorgiTypeShape } from '~types/CorgiTypes';
 
-const CorgiCardPropTypes = { corgi: CorgiTypeShape.isRequired };
+const CorgiCardPropTypes = { corgi: CorgiTypeShape.isRequired, showActions: PropTypes.bool };
 
-const CorgiCard = ({ corgi }) => {
+// const sample = {
+//   background_color: '#0a1eff',
+//   color: '#d2fc9f',
+//   created: 1614018415048601000,
+//   id: '9RJfzs3lE+flgHs4iYr/rVGBzqF/gbGxRKITUfx+H2s=',
+//   modified: 1614018415048601000,
+//   name: 'rambunctious reward',
+//   owner: 'hexus.testnet',
+//   quote: '59',
+//   rate: 'COMMON',
+//   sender: '',
+// };
+
+const CorgiCard = ({ corgi, showActions = false }) => {
   const { id, background_color, color, quote, name, rate, owner, created } = corgi;
 
   return (
     <div className='corgi-card'>
       <div className='corgi-card__header'>
         <RarityString rate={rate} />
+
+        {showActions && (
+          <Dropdown
+            title={
+              <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' viewBox='0 0 16 16'>
+                <path d='M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z' />
+              </svg>
+            }
+            listStyles={{
+              left: 'auto',
+              right: 0,
+            }}
+          >
+            {/* slug for transfer feature */}
+            <span onClick={() => console.log('Gift')}>Gift</span>
+            <span onClick={() => console.log('Trade')}>Trade</span>
+            <span onClick={() => console.log('Share')}>Share</span>
+            <span divider='true'></span>
+            <span onClick={() => console.log('Delete')}>Delete</span>
+          </Dropdown>
+        )}
       </div>
 
       <Link to={`/corgi/${id}`}>
