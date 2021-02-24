b89832f3016bd4dbf41c9343819798e6a2832024
ohexus <moskalenko.taras.d@gmail.com>
2021-02-24 16:07:14 +0200

    added NO_POSTFIX and WRONG_POSTFIX messages

--- src/constants/validation/account.js
+++ src/constants/validation/account.js
@@ -1,6 +1,8 @@
 export const USER_VALIDATION_MESSAGES = {
   OWN_ACCOUNT: 'Sorry, but you can only send corgi to another account.',
   NOT_EXIST: 'Such user does not exist.',
+  NO_POSTFIX: 'Account id must contain postfix.',
+  WRONG_POSTFIX: 'Account id postfix must match with the network id.',
 };
 
 export const NETWORK_POSTFIX = {
