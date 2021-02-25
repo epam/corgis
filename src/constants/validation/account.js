2a7f1d9727b64f578d5c6916cf48aadf19b740d8
ohexus <moskalenko.taras.d@gmail.com>
2021-02-25 10:48:50 +0200

    removed postfix check

--- src/constants/validation/account.js
+++ src/constants/validation/account.js
@@ -1,11 +1,4 @@
 export const USER_VALIDATION_MESSAGES = {
-  OWN_ACCOUNT: 'Sorry, but you can only send corgi to another account.',
+  OWN_ACCOUNT: 'Sorry, but you can only send Corgi to another account.',
   NOT_EXIST: 'Such user does not exist.',
-  NO_POSTFIX: 'Account id must contain postfix.',
-  WRONG_POSTFIX: 'Account id postfix must match with the network id.',
-};
-
-export const NETWORK_POSTFIX = {
-  default: 'testnet',
-  mainnet: 'near',
 };
