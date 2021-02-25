701ac7632201d31462c390785e427eaa6e9b9c65
ohexus <moskalenko.taras.d@gmail.com>
2021-02-25 18:15:15 +0200

    added index.js for Colorpicker

--- src/modules/common/index.js
+++ src/modules/common/index.js
@@ -1,7 +1,7 @@
 import { Button, CheckMarkButton, GenerationLink } from './buttons';
 import { BasicSpinner, CorgiSpinner } from './spinners';
 
-import Colorpicker from './Colorpicker/Colorpicker';
+import Colorpicker from './Colorpicker';
 import Dropdown from './Dropdown';
 import Egg from './Egg/Egg';
 import ExternalLink from './ExternalLink/ExternalLink';
