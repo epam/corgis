feb4f1ab042a9b84be337dfef323eb031ac81e53
ohexus <moskalenko.taras.d@gmail.com>
2021-02-25 11:41:36 +0200

    made all fields required

--- src/types/CorgiTypes.js
+++ src/types/CorgiTypes.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 import { RATES } from '~constants/corgi';
 
 export const CorgiType = {
-  id: PropTypes.string,
-  name: PropTypes.string,
-  quote: PropTypes.string,
-  color: PropTypes.string,
-  background_color: PropTypes.string,
-  rate: PropTypes.oneOf([RATES.COMMON, RATES.UNCOMMON, RATES.RARE, RATES.VERY_RARE]),
-  owner: PropTypes.string,
-  sender: PropTypes.string,
-  created: PropTypes.number,
-  modified: PropTypes.number,
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  quote: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  background_color: PropTypes.string.isRequired,
+  rate: PropTypes.oneOf([RATES.COMMON, RATES.UNCOMMON, RATES.RARE, RATES.VERY_RARE]).isRequired,
+  owner: PropTypes.string.isRequired,
+  sender: PropTypes.string.isRequired,
+  created: PropTypes.number.isRequired,
+  modified: PropTypes.number.isRequired,
 };
 
 export const CorgiTypeShape = PropTypes.shape(CorgiType);
