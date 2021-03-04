4a1109180d9e73ccec857c0c0f2122bdf609f07d
ohexus <moskalenko.taras.d@gmail.com>
2021-03-04 12:47:04 +0200

    added marketplace module

---
+++ src/modules/marketplace/page/MarketplacePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './MarketplacePage.scss';
+
+const MarketplacePage = () => {
+  return (
+    <div className='marketplace'>
+      <h1>MarketplacePage works!</h1>
+    </div>
+  );
+};
+
+export default MarketplacePage;
