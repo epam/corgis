3027180e1ed4d2f8a74024005c2d753c3d4a2ebd
ohexus <moskalenko.taras.d@gmail.com>
2021-03-05 13:02:49 +0200

    added validation to Donation input

--- src/modules/common/Donation/Donation.jsx
+++ src/modules/common/Donation/Donation.jsx
@@ -4,29 +4,45 @@ import PropTypes from 'prop-types';
 import './Donation.scss';
 
 import { Input } from '~modules/common';
+import { CORGI_VALIDATION_MESSAGES } from '~constants/validation/corgi';
 
 const DonationPropTypes = {
   label: PropTypes.string,
   afterword: PropTypes.string,
+  handleNears: PropTypes.func,
 };
 
-const Donation = ({ label = 'Donate', afterword = '' }) => {
+const Donation = ({ label = 'Donate', afterword = '', handleNears = () => {} }) => {
   const [nears, setNears] = useState(1);
 
-  const handleNears = (event) => {
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleNearsInput = (event) => {
     setNears(event.target.value);
+
+    if (nears >= 1) {
+      handleNears(event.target.value);
+    }
   };
 
   useEffect(() => {
-    console.log(nears);
+    if (nears <= 0) {
+      setErrorMessage(CORGI_VALIDATION_MESSAGES.NEARS);
+    }
   }, [nears]);
+
+  useEffect(() => {
+    if (errorMessage && errorMessage.length) {
+      setErrorMessage('');
+    }
+  }, [errorMessage]);
 
   return (
     <div className='donation'>
       <span className='donation__text'>{label}</span>
 
       <div className='donation__input'>
-        <Input type='number' min={1} value={nears} onChange={handleNears} />
+        <Input type='number' min={1} value={nears} onChange={handleNearsInput} error={errorMessage} />
       </div>
 
       <svg width='26' height='26' viewBox='0 0 26 26' fill='none' xmlns='http://www.w3.org/2000/svg'>
