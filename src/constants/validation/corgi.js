cbcc522980d2666183d8825e645641dcf876b4ef
ohexus <moskalenko.taras.d@gmail.com>
2021-03-10 14:51:24 +0200

    added BID message

--- src/constants/validation/corgi.js
+++ src/constants/validation/corgi.js
@@ -4,5 +4,6 @@ export const CORGI_VALIDATION_MESSAGES = {
   SUCCESS: 'Success!',
   LIMIT: `Name is too long, the maximum length is ${NAME_LIMIT} characters.`,
   PROFANE: "Corgi doesn't like this name and kindly asks you to be polite.",
-  NEARS: 'Not enough Ⓝ to mint Corgi.',
+  NEARS: 'Not enough Ⓝ',
+  BID: 'Your bid must be greater than the highest bid',
 };
