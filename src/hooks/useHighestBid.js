b91dacaecce9214b0d6f7e75fafe93dd58a04292
ohexus <moskalenko.taras.d@gmail.com>
2021-03-12 17:11:25 +0200

    fixed useHighestBid hook

--- src/hooks/useHighestBid.js
+++ src/hooks/useHighestBid.js
@@ -1,18 +1,20 @@
-import { useRef, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { formatToNears } from '~helpers/nears';
 
 export default function useHighestBid(forSale) {
-  const highestBid = useRef();
+  const [highestBid, setHighestBid] = useState();
 
   useEffect(() => {
     if (forSale && forSale.bids.length) {
-      highestBid.current = forSale.bids.reduce(
-        (curr, next) => (formatToNears(next.amount) > formatToNears(curr.amount) ? next : curr),
-        forSale.bids[0],
+      setHighestBid(
+        forSale.bids.reduce(
+          (curr, next) => (formatToNears(next.amount) > formatToNears(curr.amount) ? next : curr),
+          forSale.bids[0],
+        ),
       );
     }
   }, [forSale]);
 
-  return highestBid.current;
+  return highestBid;
 }
